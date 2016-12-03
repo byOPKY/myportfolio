7faecca3cbe0dfd1c2b22bffcc1307a452377dc7
byOPKY <orrillokarol@gmail.com>
2016-12-03 14:35:57 -0500

    responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	$("#datos").slideDown(3000).slideUp(3000);
 	setTimeout(function(){
 		$("#about").slideDown(4000);
@@ -6,7 +7,7 @@ $(document).ready(function(){
 		$("#galary").slideDown(6000);
 		$("#works").slideDown(7000);
 		$("#contact").slideDown(8000);
-		$("footer").slideDown(9000);
+		$("#footer").slideDown(9000);
 	},6000);
 
 
@@ -23,45 +24,55 @@ $(document).ready(function(){
 		    if(windowScrollTop>80){
 		       thirdAnimation()
 		    }
-		    if(windowScrollTop>1350){
+		    if(windowScrollTop>1050){
+		       fourthAnimation()
+		    }
+		    if(windowScrollTop>1450){
 		       firstAnimation()
 		    }
 		}
-		/*if(windowWidth<=849){
-		    if(windowScrollTop>1550){
-		       secondAnimation();
-		    }
-		    if(windowScrollTop>1050){
+		if(windowWidth<=849){
+			 if(windowScrollTop>50){
 		       thirdAnimation()
 		    }
-		    if(windowScrollTop>2450){
-		       firstAnimation()
+		    if(windowScrollTop>550){
+		       secondAnimation();
+		    }
+		    if(windowScrollTop>1350){
+		       fourthAnimation();
+		    }
+		    if(windowScrollTop>3350){
+		       firstAnimation();
 		    }
 		}
 	    if(windowWidth<=549){
-		    if(windowScrollTop>850){
-	       		thirdAnimation()
+		    if(windowScrollTop>100){
+	       		thirdAnimation();
 	    	}
-	    	if(windowScrollTop>1450) {
+	    	if(windowScrollTop>2050){
+		       secondAnimation();
+		    }
+	    	if(windowScrollTop>2950) {
 		    	fourthAnimation();
 		    }
-		    if(windowScrollTop>2950){
-		       firstAnimation()
+		    if(windowScrollTop>2200){
+		       firstAnimation();
 		    }
-		}*/
+		}
 	});
 });
 
 
 var firstAnimation = function(){			
-    $("#works .card-panel:eq(0)").delay(1000).animate({opacity:1},"slow");
-    $("#works .card-panel:eq(1)").delay(2000).animate({opacity:1},"slow");
-    $("#works .card-panel:eq(2)").delay(3000).animate({opacity:1},"slow");
-    $("#works .card-panel:eq(3)").delay(4000).animate({opacity:1},"slow");
-    $("#works .card-panel:eq(4)").delay(5000).animate({opacity:1},"slow");
-    $("#works .card-panel:eq(5)").delay(6000).animate({opacity:1},"slow");
-    $("#works .card-panel:eq(6)").delay(7000).animate({opacity:1},"slow");
-    $("#works .card-panel:eq(7)").delay(8000).animate({opacity:1},"slow");
+    $("#works .card:eq(0)").delay(1000).animate({opacity:1},"slow");
+    $("#works .card:eq(1)").delay(2000).animate({opacity:1},"slow");
+    $("#works .card:eq(2)").delay(3000).animate({opacity:1},"slow");
+    $("#works .card:eq(3)").delay(4000).animate({opacity:1},"slow");
+    $("#works .card:eq(4)").delay(5000).animate({opacity:1},"slow");
+    $("#works .card:eq(5)").delay(6000).animate({opacity:1},"slow");
+    $("#works .card:eq(6)").delay(7000).animate({opacity:1},"slow");
+    $("#works .card:eq(7)").delay(8000).animate({opacity:1},"slow");
+    $("#works .card:eq(8)").delay(9000).animate({opacity:1},"slow");
 	}
 	
 var secondAnimation = function(){
@@ -89,11 +100,17 @@ var thirdAnimation = function(){
 	}
 
 var fourthAnimation = function(){
-	$("#none .circle:eq(0)").delay(1000).animate({opacity:1, right:0},"slow");
-	$("#none .circle:eq(1)").delay(2000).animate({opacity:1, right:0},"slow");
-	$("#none .circle:eq(2)").delay(3000).animate({opacity:1, right:0},"slow");
-	$("#none .circle:eq(3)").delay(4000).animate({opacity:1, right:0},"slow");
-	$("#none .circle:eq(4)").delay(5000).animate({opacity:1, right:0},"slow");
-	$("#none .circle:eq(5)").delay(6000).animate({opacity:1, right:0},"slow");
+	$("#galary img:eq(0)").delay(500).animate({opacity:1},"slow");
+	$("#galary img:eq(1)").delay(1000).animate({opacity:1},"slow");
+	$("#galary img:eq(2)").delay(1500).animate({opacity:1},"slow");
+	$("#galary img:eq(3)").delay(2000).animate({opacity:1},"slow");
+	$("#galary img:eq(4)").delay(500).animate({opacity:1},"slow");
+	$("#galary img:eq(5)").delay(3000).animate({opacity:1},"slow");
+	$("#galary img:eq(6)").delay(1500).animate({opacity:1},"slow");
+	$("#galary img:eq(7)").delay(4000).animate({opacity:1},"slow");
+	$("#galary img:eq(9)").delay(4500).animate({opacity:1},"slow");
+	$("#galary img:eq(10)").delay(2000).animate({opacity:1},"slow");
+	$("#galary img:eq(11)").delay(1500).animate({opacity:1},"slow");
+	$("#galary img:eq(12)").delay(4000).animate({opacity:1},"slow");
 }
 
